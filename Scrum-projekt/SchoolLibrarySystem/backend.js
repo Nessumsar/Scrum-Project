09b6fde1c6e880bb5c7ff80df236e69b49f57c41
crazvik <crazvik@gmail.com>
2019-12-20 11:07:39 +0100

    Fixed backend for bootstrap

--- Scrum-projekt/SchoolLibrarySystem/backend.js
+++ Scrum-projekt/SchoolLibrarySystem/backend.js
@@ -6,10 +6,12 @@ function renderBooks() {
     for(let i=0; i<books.length; i++) {
         var newRow = document.createElement('div');
         var column = document.createElement('div');
-        newRow.id = 'row';
-        column.id = 'col-sm';
+        newRow.className = 'row';
+        column.className = 'col-sm';
         if(i===0 || i%3===0) {
             for(let j=i; j<i+3; j++) {
+                column = document.createElement('div');
+                column.className = 'col-sm';
                 column.append(books[j].Title);
                 newRow.append(column);
             }
