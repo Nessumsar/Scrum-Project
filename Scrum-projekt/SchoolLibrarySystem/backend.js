629d449d07108d72d0d69443a5c98d6b9218f183
crazvik <crazvik@gmail.com>
2019-12-22 18:06:02 +0100

    Search rendering

--- Scrum-projekt/SchoolLibrarySystem/backend.js
+++ Scrum-projekt/SchoolLibrarySystem/backend.js
@@ -10,9 +10,15 @@ function renderBooks() {
         column.className = 'col-sm';
         if(i===0 || i%3===0) {
             for(let j=i; j<i+3; j++) {
+                var picture = document.createElement('img');
+                picture.src = books[j].Picture;
                 column = document.createElement('div');
                 column.className = 'col-sm';
+                column.append(picture);
+                column.append(document.createElement('br'));
                 column.append(books[j].Title);
+                column.append(document.createElement('br'));
+                column.append("Remaining: " + books[j].Remaining);
                 newRow.append(column);
             }
             document.getElementById('container').append(newRow);
