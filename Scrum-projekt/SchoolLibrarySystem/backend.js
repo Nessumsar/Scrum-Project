b09cdddf5fa8cc47a24ff8779de01d7abf1f1d76
crazvik <crazvik@gmail.com>
2019-12-23 11:03:50 +0100

    Backend för val av bok

--- Scrum-projekt/SchoolLibrarySystem/backend.js
+++ Scrum-projekt/SchoolLibrarySystem/backend.js
@@ -1,5 +1,10 @@
 window.onload = function() {
     renderBooks();
+    document.getElementById("search").addEventListener("click", searchBooks);
+    for(let i=0; i<document.getElementsByClassName("col-sm").length; i++) {
+        document.getElementsByClassName("col-sm")[i].addEventListener("click", selectedBook);
+    }
+    
 }
 
 function renderBooks() {
@@ -12,6 +17,7 @@ function renderBooks() {
             for(let j=i; j<i+3; j++) {
                 var picture = document.createElement('img');
                 picture.src = books[j].Picture;
+                picture.id = "image";
                 column = document.createElement('div');
                 column.className = 'col-sm';
                 column.append(picture);
@@ -25,3 +31,32 @@ function renderBooks() {
         }
     }
 }
+
+function searchBooks() {
+    var input, filter, grid, li, a, i, txtValue;
+    input = document.getElementById("search-bar");
+    filter = input.value.toUpperCase();
+    grid = document.getElementById("container");
+    li = grid.getElementsByTagName("Text");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("text")[0];
+        txtValue = a.textContent || a.innerText;
+        console.log(txtValue.toUpperCase().indexOf(filter));
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function selectedBook(e) {
+    let event = e.target;
+    if(event.style.boxShadow==="darkblue 0px 0px 20px 2px") {
+        event.style.boxShadow = "0px 0px 10px 2px black";
+    }
+    else {
+        event.style.boxShadow = "0px 0px 20px 2px darkblue";
+        
+    }
+}        
